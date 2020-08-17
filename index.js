fd49388b2cf08fda801c73a581ce122d5d9d64ad
justin-jones <52481185+justinemanueljones@users.noreply.github.com>
2020-08-17 16:58:09 -0400

    mvp

--- index.js
+++ index.js
@@ -17,10 +17,7 @@
 function processFirstItem(stringList, callback) {
   return callback(stringList[0])
 }
-
 // ⭐️ Example Challenge END ⭐️
-
-
 ///// M V P ///////
 
 /* Task 1: `counterMaker`
@@ -28,10 +25,16 @@ function processFirstItem(stringList, callback) {
  * 
  * 1. What is the difference between counter1 and counter2?
  * 
+ * counter 1 is block scope and counter 2 is global scope
+ * 
  * 2. Which of the two uses a closure? How can you tell?
+ * 
+ * counter 1 is a closure it declares in the function 
  * 
  * 3. In what scenario would the counter1 code be preferable? In what scenario would counter2 be better? 
  *
+ * almost all the time
+ * 
 */
 
 // counter1 code
@@ -56,11 +59,12 @@ function counter2() {
 
 Write a function called `inning` that returns a random number of points that a team scored in an inning. This should be a whole number between 0 and 2. */
 
-function inning(/*Code Here*/){
-
-    /*Code Here*/
-
+function inning(num1, num2, cb){
+  return cb(num1, num2);
 }
+const add = (num1, num2) => num1 + num2;
+console.log(inning(0, 2, add));
+
 
 /* Task 3: finalScore()
 
@@ -75,12 +79,22 @@ finalScore(inning, 9) might return:
 }
 
 */ 
-
-function finalScore(/*code Here*/){
-
-  /*Code Here*/
-
+const finalScore = function(i1, i2, cb){
+  return cb(i1, i2);
 }
+
+// call back functions
+const homeTeam = function(i1, i2){
+  return `home:${i1} final score was ${i2} `;
+}
+
+const awayTeam = function(i1, i2){
+  return `away:${i1} final score was ${i2} `;
+}
+
+console.log(finalScore('inning 9', '11', homeTeam));
+console.log(finalScore('inning 9', '5', awayTeam));
+
 
 /* Task 4: 
 
@@ -102,9 +116,37 @@ and returns the score at each pont in the game, like so:
 9th inning: awayTeam - homeTeam
 Final Score: awayTeam - homeTeam */
 
-
-function scoreboard(/* CODE HERE */) {
-  /* CODE HERE */
-}
+const scoreboard = function(i1, i2, cb){
+  return cb(i1, i2);}
+const repeat1 = function(i1, i2){
+  return `1st inning: ${i1} awayTeam ${i2} homeTeam `;}
+const repeat2 = function(i1, i2){
+  return `2nd inning: ${i1} awayTeam ${i2} homeTeam `;}
+const repeat3 = function(i1, i2){
+  return `3rd inning: ${i1} awayTeam ${i2} homeTeam `;}
+const repeat4 = function(i1, i2){
+  return `4th inning: ${i1} awayTeam ${i2} homeTeam `;}
+const repeat5 = function(i1, i2){
+  return `5th inning: ${i1} awayTeam ${i2} homeTeam `;}
+const repeat6 = function(i1, i2){
+  return `6th inning: ${i1} awayTeam ${i2} homeTeam `;}
+const repeat7 = function(i1, i2){
+  return `7th inning: ${i1} awayTeam ${i2} homeTeam `;}
+const repeat8 = function(i1, i2){
+  return `8th inning: ${i1} awayTeam ${i2} homeTeam `;}
+const repeat9 = function(i1, i2){
+  return `9th inning: ${i1} awayTeam ${i2} homeTeam `;}
+const repeatFinal = function(i1, i2){
+  return `Final Score: ${i1} awayTeam ${i2} homeTeam `;}
+console.log(scoreboard('1 ', '2', repeat1));
+console.log(scoreboard('2 ', '4', repeat2));
+console.log(scoreboard('3 ', '5', repeat3));
+console.log(scoreboard('6 ', '5', repeat4));
+console.log(scoreboard('7 ', '7', repeat5));
+console.log(scoreboard('7 ', '7', repeat6));
+console.log(scoreboard('8 ', '7', repeat7));
+console.log(scoreboard('9 ', '8', repeat8));
+console.log(scoreboard('9 ', '8', repeat9));
+console.log(scoreboard('9 ', '8', repeatFinal));
 
 
